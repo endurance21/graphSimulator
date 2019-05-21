e2a85b15c48447a7cb1593fc8f201d63af73c97c
endurance21 <43696525+endurance21@users.noreply.github.com>
2019-05-21 18:04:04 +0530

    update 1.01
    added option for wave color ,
    wave velocity ,
    canvas size made small to boost performance ,
    cancel animation at any time and then resume it feature added

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-var wave = {
-    y:innerHeight/2,
-    frequency:0.01,
-    amplitude:200,
-    phase:3.14,
-    doClearRect:false
-};
 const canvas  = document.querySelector('canvas');
-canvas.width = innerWidth ;
-canvas.height = innerHeight;
 const ctx = canvas.getContext('2d');
-
-
+var wave = {
+    y:canvas.height/2,
+    frequency:0.01,
+    amplitude:canvas.height/5,
+    velocity:0.09,
+};
+var strokeColor = {
+    r:255 ,
+    g:36,
+    b:22 
+};
+var animation = {
+    cancelAnimation:false ,
+    doClearRect:true 
+}
 var gui = new dat.GUI();
-gui.add(wave , 'y' ,0 ,innerHeight,0.1);
-gui.add(wave ,'frequency',-0.1,0.1 ,0.0001);
-gui.add(wave ,'amplitude' ,-300, 300 ,0.9);
-gui.add(wave ,'phase' ,-3.14 , 100 )
-gui.add(wave , 'doClearRect');
+var increament = 2 ;
+var animationref = null ;
+
+
+canvas.width = innerWidth/2;
+canvas.height = innerHeight/2;
+
+const waveFolder = gui.addFolder('WAVE');   
+waveFolder.add(wave , 'y' ,0 ,canvas.height,0.1);
+waveFolder.add(wave ,'frequency',-0.1,0.1 ,0.0001);
+waveFolder.add(wave ,'amplitude' ,-300, 300 ,0.9);
+waveFolder.add(wave ,'velocity' ,-2, 2 ,0.001);
+
+const strokeFolder = gui.addFolder('COLOR');
+strokeFolder.add(strokeColor ,'r',0,255)
+strokeFolder.add(strokeColor ,'g',0,255)
+strokeFolder.add(strokeColor ,'b',0,255)
+
+const animationFolder  = gui.addFolder('ANIMATION_CONTROL');
+animationFolder.add(animation , 'cancelAnimation').onFinishChange(()=>{
+    if(animation.cancelAnimation)
+    cancelAnimationFrame(animationref);
+    else
+    animate();
+});
+animationFolder.add(animation, 'doClearRect');
+
+
 function animate(){
-    window.requestAnimationFrame(animate) ;
-    if(wave.doClearRect){
-    ctx.clearRect(0,0,innerWidth,innerHeight); 
-    }
-    ctx.moveTo(0,innerHeight/2) ;
-    ctx.beginPath();
-    for(let i = 0 ; i< canvas.width ; i++ ) {
-    ctx.lineTo( i , wave.y + Math.sin(i*wave.frequency + wave.phase)*wave.amplitude ) ;
-    ctx.stroke();
-   }
+    animationref = requestAnimationFrame(animate) ;
+    if(animation.doClearRect)
+        ctx.clearRect(0,0,canvas.width,canvas.height); 
+   else{
+       ctx.fillStyle = 'rgba(0,0,0,0.01)';
+       ctx.fillRect(0,0,canvas.width,canvas.height);
+     }
+  ctx.moveTo(0,canvas.height/2);
+  ctx.beginPath();
+  for(let i = 0 ; i< canvas.width ; i++ ) {
+  ctx.lineTo( i , wave.y + Math.sin(i*(wave.frequency) + increament)*(wave.amplitude)) ;
+
+  ctx.strokeStyle = "rgb("+ strokeColor.r +"," + strokeColor.g + ","+strokeColor.b+") ";
+  ctx.stroke();
+ }
+ increament+=Math.abs(Math.sin(wave.velocity/5));
+  
 }
 animate();
 
